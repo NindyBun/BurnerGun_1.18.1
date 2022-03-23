c658c8ad2fea5b1047cb5c90ac399e869618d81e
NindyBun <69482823+NindyBun@users.noreply.github.com>
2022-03-23 00:11:21 -0700

    hm

--- src/main/java/com/nindybun/burnergun/common/items/upgrades/Vein_Miner/VeinMiner.java
+++ src/main/java/com/nindybun/burnergun/common/items/upgrades/Vein_Miner/VeinMiner.java
@@ -40,21 +40,57 @@ public class VeinMiner extends UpgradeCard {
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
         if (!level.isClientSide){
-            ListTag filterTag = stack.getOrCreateTag().getList(VEIN_MINER_FILTER, Tag.TAG_COMPOUND);
-            ListTag filterTagString = new ListTag();
-            filterTag.forEach(tag -> {
-                filterTagString.add(((CompoundTag)tag).getString(BLOCK))
+            ListTag oldTag = stack.getOrCreateTag().getList(VEIN_MINER_FILTER, Tag.TAG_COMPOUND);
+            LOGGER.info("OLD >> " + oldTag);
+
+            ListTag oldTagString = new ListTag();
+            oldTag.forEach(tag -> {
+                CompoundTag tTag = (CompoundTag) tag;
+                CompoundTag ttTag = new CompoundTag();
+                ttTag.putString(BLOCK, tTag.getString(BLOCK));
+                oldTagString.add(ttTag);
             });
-            List<Block> blockList = ForgeRegistries.BLOCKS.getValues().stream().collect(Collectors.toList());
-            blockList.forEach(block -> {
+            
+            ListTag newTag = new ListTag();
+            ForgeRegistries.BLOCKS.getValues().stream().collect(Collectors.toList()).forEach(block -> {
                 CompoundTag tag = new CompoundTag();
                 tag.putString(BLOCK, block.toString());
-                if (!filterTag.contains(tag)){
+                if (oldTagString.contains(tag))
+                    oldTag.forEach(oTag -> {
+                        CompoundTag ooTag = (CompoundTag) oTag;
+                        LOGGER.info(ooTag.getString(BLOCK) + " >> "+ tag.getString(BLOCK) + " >> " + ooTag.equals(tag));
+                        if (ooTag.getString(BLOCK).equals(tag.getString(BLOCK))){
+                            tag.putBoolean(WHITELIST, ooTag.getBoolean(WHITELIST));
+                            return;
+                        }
+                    });
+                else
                     tag.putBoolean(WHITELIST, false);
-                    filterTag.add(tag);
-                }
+                newTag.add(tag);
             });
-            stack.getOrCreateTag().put(VEIN_MINER_FILTER, filterTag);
+            stack.getOrCreateTag().put(VEIN_MINER_FILTER, newTag);
+
+           /*if (!oldTagString.isEmpty()){
+                ListTag newTag = new ListTag();
+                newTagString.forEach(tag -> {
+                    CompoundTag nTag = (CompoundTag) tag;
+                    oldTagString.forEach(oTag -> {
+                        CompoundTag ooTag = (CompoundTag) oTag;
+                        nTag.putBoolean(WHITELIST, ooTag.equals(nTag) ? ooTag.getBoolean(WHITELIST) : false);
+                        newTag.add(newTag);
+                    });
+                });
+                stack.getOrCreateTag().put(VEIN_MINER_FILTER, newTag);
+            }else{
+
+                stack.getOrCreateTag().put(VEIN_MINER_FILTER, newTagString.forEach(tag -> {
+                    CompoundTag nTag = (CompoundTag) tag;
+                    nTag.putBoolean(WHITELIST, false);
+
+                }));
+            }*/
+
+            LOGGER.info("NEW >> " + stack.getOrCreateTag().getList(VEIN_MINER_FILTER, Tag.TAG_COMPOUND));
             //ModScreens.openFilterListScreen(stack);
         }
         return InteractionResultHolder.success(stack);
