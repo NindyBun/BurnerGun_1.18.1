42c1a4771321c4bcf233ea3eb6de7db5dc55e00a
NindyBun <andyco5665@gmail.com>
2022-03-23 20:05:03 -0700

    hmm

--- src/main/java/com/nindybun/burnergun/client/screens/FilterListScreen.java
+++ src/main/java/com/nindybun/burnergun/client/screens/FilterListScreen.java
@@ -10,11 +10,16 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.ItemStack;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.opengl.GL11;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class FilterListScreen extends Screen implements Scrollable {
+public class FilterListScreen extends Screen {
+    private final static Logger LOGGER = LogManager.getLogger();
+    private int red;
     protected FilterListScreen(ItemStack stack) {
         super(new TextComponent("Title"));
     }
@@ -29,8 +34,13 @@ public class FilterListScreen extends Screen implements Scrollable {
     }
 
     @Override
-    public boolean mouseScrolled(double p_94686_, double p_94687_, double p_94688_) {
-        return super.mouseScrolled(p_94686_, p_94687_, p_94688_);
+    public boolean mouseScrolled(double scrollX, double scrollY, double scrollDelta) {
+            red += (int)scrollDelta;
+            if (red >= 200)
+                red = 200;
+            else if (red <= 10)
+                red = 10;
+        return super.mouseScrolled(scrollX, scrollY, scrollDelta);
     }
 
     @Override
@@ -48,32 +58,16 @@ public class FilterListScreen extends Screen implements Scrollable {
         //Gives us the darkened background
         this.renderBackground(matrixStack);
         super.render(matrixStack, mouseX, mouseY, ticks_);
-        TranslatableComponent string = new TranslatableComponent("tooltip." + BurnerGun.MOD_ID + ".screen.filter");
-        drawString(matrixStack, Minecraft.getInstance().font, string, (width/2)- StringUtil.getStringPixelLength(string.getString())/2, 20, Color.WHITE.getRGB());
-        }
+        fill(matrixStack, 10, 10, 10+50, 10+200, Color.WHITE.hashCode());
+        GL11.glPushMatrix();
+        GL11.glEnable(GL11.GL_SCISSOR_TEST);
+        GL11.glTranslatef(0, red, 0);
+        GL11.glScissor(10, 10+red, 60, 60);
+        fill(matrixStack, 10, 10, 60, 10, new Color(1f, 0f, 0f).hashCode());
+        GL11.glDisable(GL11.GL_SCISSOR_TEST);
+        GL11.glPopMatrix();
 
-    @Override
-    public Dimension getPreferredScrollableViewportSize() {
-        return null;
-    }
-
-    @Override
-    public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
-        return 0;
-    }
-
-    @Override
-    public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
-        return 0;
-    }
-
-    @Override
-    public boolean getScrollableTracksViewportWidth() {
-        return false;
-    }
-
-    @Override
-    public boolean getScrollableTracksViewportHeight() {
-        return false;
+//        TranslatableComponent string = new TranslatableComponent("tooltip." + BurnerGun.MOD_ID + ".screen.filter");
+//        drawString(matrixStack, Minecraft.getInstance().font, string, (width / 2) - StringUtil.getStringPixelLength(string.getString()) / 2, 20, Color.WHITE.getRGB());
     }
 }
