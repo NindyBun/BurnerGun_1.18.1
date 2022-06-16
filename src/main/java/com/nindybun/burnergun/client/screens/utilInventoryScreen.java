8bf137562771336510e125377b9f184fe901b66f
NindyBun <andyco5665@gmail.com>
2022-06-15 18:30:05 -0700

    hmm

--- src/main/java/com/nindybun/burnergun/client/screens/utilInventoryScreen.java
+++ src/main/java/com/nindybun/burnergun/client/screens/utilInventoryScreen.java
@@ -5,10 +5,13 @@ import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
-public class utilInventoryScreen extends EffectRenderingInventoryScreen {
+public class utilInventoryScreen extends EffectRenderingInventoryScreen<utilInventoryScreen.ChestInventoryMenu> {
     public utilInventoryScreen(ItemStack utilInv) {
         super(new TextComponent("Titile"));
     }
@@ -17,4 +20,17 @@ public class utilInventoryScreen extends EffectRenderingInventoryScreen {
     protected void renderBg(PoseStack p_97787_, float p_97788_, int p_97789_, int p_97790_) {
 
     }
+
+    static class ChestInventoryMenu extends AbstractContainerMenu {
+
+        protected ChestInventoryMenu(Player player) {
+            super((MenuType<?>)null, 0);
+        }
+
+        @Override
+        public boolean stillValid(Player player) {
+            return player.isAlive();
+        }
+    }
+
 }
